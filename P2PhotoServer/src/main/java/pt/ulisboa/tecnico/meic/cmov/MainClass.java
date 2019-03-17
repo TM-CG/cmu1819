5f2a9c291bc967f12ab4be8cd96ca045c50e6477
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-03-17 15:19:43 +0000

    Adds initial server pom configs

---
+++ P2PhotoServer/src/main/java/pt/ulisboa/tecnico/meic/cmov/MainClass.java
@@ -0,0 +1,7 @@
+package pt.ulisboa.tecnico.meic.cmov;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("This is the P2Photo-Server running!");
+    }
+}

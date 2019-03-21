cc6655f0db05749be436bbe92c43ce0b12dc4e56
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-03-21 15:25:26 +0000

    Adds signup and login logic to server

--- P2PhotoServer/src/main/java/pt/ulisboa/tecnico/meic/cmov/Server.java
+++ P2PhotoServer/src/main/java/pt/ulisboa/tecnico/meic/cmov/Server.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.meic.cmov;
 
+import javafx.util.Pair;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,6 +15,7 @@ import java.util.List;
 public class Server {
 
     private List<User> users;
+    private List<Pair<String, String>> loggedInUsers;
 
     /** Socket related **/
     private ServerSocket serverSocket;
@@ -23,7 +26,8 @@ public class Server {
     private static final int SERVER_PORT = 10000;
 
     public Server() {
-        this.users = new ArrayList<User>();
+        this.users = new ArrayList<>();
+        this.loggedInUsers = new ArrayList<>();
     }
 
     public void addUser(User user) {
@@ -38,6 +42,22 @@ public class Server {
         return false;
     }
 
+    public String usernameIsLoggedOn(String username) {
+        for (Pair<String, String> user: this.loggedInUsers) {
+            if (user.getKey().equals(username))
+                return user.getValue();
+        }
+        return null;
+    }
+
+    public User getUserByUsername(String username) {
+        for (User user: users) {
+            if (user.getUsername().equals(username))
+                return user;
+        }
+        return null;
+    }
+
     /** === SOCKET RELATED === **/
 
     public void initSocket() {
@@ -47,10 +67,16 @@ public class Server {
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            while(true) {
-                String message = in.readLine();
+            String message;
+            List<String> args;
 
-                System.out.println("Client response: " + message);
+            while(true) {
+                message = in.readLine();
+
+                args = parseInstruction(message);
+
+                out.println(processInstruction(args));
+
             }
 
         } catch(IOException e) {
@@ -72,12 +98,73 @@ public class Server {
 
     private List<String> parseInstruction(String instruction) {
         List<String> args = null;
-        if (instruction.startsWith("LOGIN")) {
+        if (instruction.startsWith("LOGIN") || instruction.startsWith("SIGNUP") || (instruction.startsWith("LOGOUT")) || (instruction.startsWith("ALB-AUP"))) {
             args = Arrays.asList(instruction.split(" "));
+        }
+        return args;
+    }
 
+    private String processInstruction(List<String> args) {
+
+        try {
+            String instruction = args.get(0);
+            String username;
+            String password;
+            String sessionId;
+            User user;
+
+            switch (instruction) {
+                case "LOGIN":
+                    username = args.get(1);
+                    password = args.get(2);
+
+                    if (!usernameExists(username)) {
+                        System.out.println("** LOGIN: User " + username + " does not exists");
+                        return "NOK 1";
+                    } else {
+                        user = getUserByUsername(username);
+                        if (!user.getPassword().equals(password)) {
+                            System.out.println("** LOGIN: User " + username + " failed!");
+                            return "NOK 2";
+                        } else {
+
+                            sessionId = usernameIsLoggedOn(username);
+
+                            if (sessionId == null) {
+                                sessionId = Long.toHexString(Double.doubleToLongBits(Math.random()));
+                                this.loggedInUsers.add(new Pair<>(username, sessionId));
+                            }
+                            return "OK " + sessionId;
+
+
+                        }
+
+                    }
+
+                case "SIGNUP":
+                    username = args.get(1);
+                    password = args.get(2);
+
+
+                    //Check if user exists
+                    if (usernameExists(username)) {
+                        System.out.println("** SIGNUP: User " + username + " already exists");
+                        return "NOK 3";
+                    } else {
+                        //Adds user
+                        users.add(new User(username, password));
+                        System.out.println("** SIGNUP: Successfully added user " + username);
+                        return "OK";
+                    }
+
+                case "LOGOUT":
+
+            }
+        } catch(Exception e) {
+            return "ERR";
         }
 
-        return args;
+        return null;
     }
 
 

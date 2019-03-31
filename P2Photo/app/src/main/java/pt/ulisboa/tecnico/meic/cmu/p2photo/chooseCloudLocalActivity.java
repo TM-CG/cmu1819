7debed79b674496c1f03e41f825a24ea1611ea69
mbelem <mbelem@sysmail.me>
2019-03-31 19:06:36 +0100

    adds Choose between local and cloud activity closes #17

---
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/meic/cmu/p2photo/chooseCloudLocalActivity.java
@@ -0,0 +1,15 @@
+package pt.ulisboa.tecnico.meic.cmu.p2photo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pt.ulisboa.tecnico.meic.cmu.p2photo.R;
+
+public class chooseCloudLocalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_cloud_local);
+    }
+}

5baa23884cb57487101a71a9fde3cfe20b6e7fda
mbelem <mbelem@sysmail.me>
2019-04-01 15:04:41 +0100

    adds addUser from mainMenu activity closes #22

--- P2Photo/app/src/main/java/pt/ulisboa/tecnico/meic/cmu/p2photo/AddUser.java
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/meic/cmu/p2photo/AddUser.java
@@ -3,11 +3,13 @@ package pt.ulisboa.tecnico.meic.cmu.p2photo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class AddUser extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
     }
 }
---
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/meic/cmu/p2photo/addUserFromMainMenu.java
@@ -0,0 +1,96 @@
+package pt.ulisboa.tecnico.meic.cmu.p2photo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class addUserFromMainMenu extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String> itemsAdapter;
+    private ListView lvItems;
+
+    private ArrayList<String> items2;
+    private ArrayAdapter<String> itemsAdapter2;
+    private ListView lvItems2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_user_from_main_menu);
+
+        items = new ArrayList<String>();
+
+        lvItems = (ListView) findViewById(R.id.usersSearch);
+
+        itemsAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+
+        lvItems.setAdapter(itemsAdapter);
+
+        items2 = new ArrayList<String>();
+
+        lvItems2 = (ListView) findViewById(R.id.usersBeingAdded);
+
+        itemsAdapter2 = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items2);
+
+        lvItems2.setAdapter(itemsAdapter2);
+
+        setupListViewListener();
+        createUsersTest();
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        addUser(pos);
+
+                        return true;
+                    }
+
+                });
+        lvItems2.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        items2.remove(pos);
+                        itemsAdapter2.notifyDataSetChanged();
+
+                        return true;
+                    }
+
+                });
+    }
+
+    private void createUsersTest(){
+        itemsAdapter.add("João");
+        itemsAdapter.add("Carlos");
+        itemsAdapter.add("Alberto");
+        itemsAdapter.add("Gorila");
+        itemsAdapter.add("Pulpo");
+        itemsAdapter.add("Pardal");
+        itemsAdapter.add("Vitor");
+        itemsAdapter.add("Titas");
+        itemsAdapter.add("Miguel");
+        itemsAdapter.add("Samora");
+    }
+
+    private void addUser(int pos){
+        // Remove the item within array at position
+        if(!items2.contains(items.get(pos))) {
+            items2.add(items.get(pos));
+            // Refresh the adapter
+            itemsAdapter2.notifyDataSetChanged();
+            // Return true consumes the long click event (marks it handled)
+        }
+    }
+}

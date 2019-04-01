c4164ac285b7ea8aa2199392d2845bb8b1dc3c1f
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-04-01 14:12:59 +0100

    Adds log activity closes #25

---
+++ P2Photo/app/src/main/java/pt/ulisboa/tecnico/meic/cmu/p2photo/LogActivity.java
@@ -0,0 +1,12 @@
+package pt.ulisboa.tecnico.meic.cmu.p2photo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LogActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+    }
+}
